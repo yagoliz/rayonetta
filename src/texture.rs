8191ed0743c0b3a1fc842ac5eb3aef15ba5b4762
Yago Lizarribar <yago.lizarribar@gmail.com>
2024-09-13 16:17:07 +0200

    fix some warnings on image texture file

--- src/texture.rs
+++ src/texture.rs
@@ -87,7 +87,7 @@ impl ImageTexture {
 }
 
 impl Texture for ImageTexture {
-    fn value(&self, u: f64, v: f64, p: Point3) -> Color {
+    fn value(&self, u: f64, v: f64, _p: Point3) -> Color {
         if self.image.height() <= 0 {
             return Color::new(0.0, 1.0, 1.0);
         }
